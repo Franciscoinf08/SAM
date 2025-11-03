d2363d5fa9564d45ae3c9766109b470f07a3a2b9
Mateus0Vasconcelos <mateus17vasc@gmail.com>
2025-11-02 22:37:46 -0300

    Adição das classes de domínio

---
+++ sam-project/sam-model/src/main/java/sam/model/domain/Usuario.java
@@ -0,0 +1,33 @@
+package sam.model.domain;
+
+import sam.model.domain.util.UsuarioTipo;
+
+public class Usuario {
+    private String nome;
+    private final String cpf;
+    private String email;
+    private String senha;
+    private UsuarioTipo tipo;
+
+    public Usuario(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() { return senha; }
+
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getCPF() {return cpf;}
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) { this.email = email; }
+
+    public String getSenha() { return senha; }
+
+    public void setSenha(String senha) { this.senha = senha; }
+
+    public UsuarioTipo getTipo() { return tipo; }
+
+    public void setTipo(UsuarioTipo tipo) { this.tipo = tipo; }
+}
