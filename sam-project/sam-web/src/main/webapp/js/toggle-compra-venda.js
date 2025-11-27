664b4121fb028d96da92947da7ba848b7029f535
Mateus0Vasconcelos <mateus17vasc@gmail.com>
2025-11-27 00:39:46 -0300

    Pequeno ajuste no input da data de expiração do formulário de cadastro de transações #19

--- sam-project/sam-web/src/main/webapp/js/toggle-compra-venda.js
+++ sam-project/sam-web/src/main/webapp/js/toggle-compra-venda.js
@@ -5,7 +5,8 @@ const dataExpiracaoEl = document.querySelector(".data-expiracao");
 tipoInputEl.addEventListener("change", () => {
     if (tipoInputEl.value === "COMPRA") {
         bonusEl.style.display = "block";
-        dataExpiracaoEl.innerHTML = "<input name=\"dataExpiracao\" type=\"date\" value=\"<%= LocalDate.now().plusDays(1).toString() %>\" required>"
+        let amanha = new Date(new Date().setDate(new Date().getDate() + 1)).toISOString().split('T')[0]
+        dataExpiracaoEl.innerHTML = `Data de Expiração:<input name=\"dataExpiracao\" type=\"date\" value=\"${amanha}\" required>`;
     } else {
         bonusEl.style.display = "none";
         dataExpiracaoEl.innerHTML = "";
